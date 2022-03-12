28f205de13fa49098411873105a40ace95eedccf
Conda <161511@qq.com>
2022-03-12 16:27:35 +0800

    four commit

--- src/main/java/com/cn/demo.java
+++ src/main/java/com/cn/demo.java
@@ -5,6 +5,5 @@ public class demo {
         System.out.println("hello , git!");
         System.out.println("hello , git2!");
         System.out.println("hello , git3!");
-        System.out.println("hello , hot-fix!");
     }
 }
